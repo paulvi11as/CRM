3cfa8a312c67bd9db2fc2113775e0b9e90397c11
Paul Villas <pvillas@academic.rrc.ca>
2024-11-05 11:03:07 -0600

    Added Samle customers and validations for the model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,6 @@
 class Customer < ApplicationRecord
   has_one_attached :image
+
+  validates :full_name, presence: true
+  validates :phone_number, presence: true, format: { with: /\A\d{3}-\d{3}-\d{4}\z/, message: "must be in the format XXX-XXX-XXXX" }
 end
